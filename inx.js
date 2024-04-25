b8831686756543a7c6aeb5d5aabce0ee867abfa5
Brz2521 <MACETs.J2911@gmail.com>
2024-04-25 01:15:22 -0400

    shirt selection

--- inx.js
+++ inx.js
@@ -1,1 +1,15 @@
-console.log("Hi");
+/* This part selects a piece of clothing and puts 
+it on the female character */
+ const shirt = document.getElementById("shirt");
+ const locaShirt = document.getElementById("shirt");
+
+ shirt.addEventListener("click", selectedShirt);
+
+ function selectedShirt(){
+    console.log("shirt selected");
+
+    locaShirt.style.postion = "absolute";
+    locaShirt.style.top = "198px";
+    locaShirt.style.left = "92px";
+
+ }

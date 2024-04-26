34be11c800e89329cdcdb101563abc82cd8a85c6
Brz2521 <MACETs.J2911@gmail.com>
2024-04-26 16:34:59 -0400

    click count method (if-else), hair added

--- inx.js
+++ inx.js
@@ -1,30 +1,28 @@
 /* This part selects a piece of clothing and puts 
 it on the female character */
  const shirt = document.getElementById("shirt");
- const newLocaShirt = document.getElementById("shirt");
- /*Braces {} are used to encapsulate these property-value pairs(top and left) into a single object.*/
- const originalShirtLocation = {top: "150px", left: "370px"};
+ const origLocaShirt = {top:"150px", left: "370px"};
+ let clickCount = 0;
 
  shirt.addEventListener("click", selectedShirt);
 
  function selectedShirt(){
     console.log("shirt selected");
+    clickCount++;
+    console.log(clickCount);
     /* This is the position of the where the shirt
     will go on the female character*/
-    newLocaShirt.style.postion = "absolute";
-    newLocaShirt.style.top = "198px";
-    newLocaShirt.style.left = "92px";
+    shirt.style.postion = "absolute";
+    shirt.style.top = "198px";
+    shirt.style.left = "92px";
 
-    /*Event listener to the character image*/
-    const returnShirt = document.getElementById("shirt");
-    returnShirt.addEventListener("click", returnShirt, {once:true}); // Listen for one click and then remove the event listener
+    if(clickCount === 2){
+      shirt.style.top = "150px";
+      shirt.style.left = "370px";
+      shirt.style.height = "155px";
+      clickCount = 0;
 
+    }
  }
 
- function returnShirt(){
-   console.log("Character clicked");
 
-   shirt.style.position = "absolute";
-   shirt.style.top = originalShirtLocation.top;
-   shirt.style.left = originalShirtLocation.left;
- }
